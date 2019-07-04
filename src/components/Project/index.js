c26b1a01b058010331ac026cbe00e388b657717f
YizYah <yyakovson613@gmail.com>
2019-07-04 14:14:24 +0300

    moving todo source to the Todos component

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Source } from 'no-stack';
 
-import Todo from '../Todo';
+import Todos from '../Todos';
 import CreateTodoForm from '../CreateTodoForm';
 
 import { SOURCE_TODOSOURCE_ID } from '../../config';
@@ -16,54 +16,19 @@ const ProjectStyleWrapper = styled.div`
   box-shadow: 5px 5px 10px #888888;
 `;
 
-const Todos = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
-`;
-
 function Project({ project }) {
-  const parameters = {
-    currentProjectId: project.id
-  };
 
   return (
     <ProjectStyleWrapper>
       <h3>{project.value}</h3>
-      <Source
-        id={SOURCE_TODOSOURCE_ID}
-        query={TODOS_FOR_CURRENT_PROJECT_SOURCE_QUERY}
-        typeRelationships={TODOS_FOR_CURRENT_PROJECT_RELATIONSHIPS}
-        parameters={parameters}
-      >
-        {({ loading, error, data, updateSourceAfterCreateAction, updateSourceAfterUpdateAction}) => {
-          if (loading) return 'Loading...';
-
-          if (error) return `Error: ${error.graphQLErrors}`;
-
-          const todos = data.sourceData.map(el => ({
-            ...el.instance,
-            isCompleted: el.children[0].instance,
-          }));
 
           return (
             <>
               <CreateTodoForm projectId={project.id} onAdd={updateSourceAfterCreateAction} />
-              <Todos>
-                {todos.map(todo => (
-                  <Todo
-                    key={todo.id}
-                    id={todo.id}
-                    name={todo.value}
-                    isCompleted={todo.isCompleted}
-                    onUpdate={updateSourceAfterUpdateAction}
-                  />
-                ))}
-              </Todos>
+              <Todos project = { project } />
             </>
           );
         }}
-      </Source>
     </ProjectStyleWrapper>
   )
 }
---
+++ src/components/Todos/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Source } from 'no-stack';
+
+import Todo from '../Todo';
+import {SOURCE_TODOSOURCE_ID} from "../../config";
+import {TODOS_FOR_CURRENT_PROJECT_RELATIONSHIPS, TODOS_FOR_CURRENT_PROJECT_SOURCE_QUERY} from "../../source-props/todo";
+
+const StyleWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+`;
+
+
+function Todos({ project }) {
+    const parameters = {
+        currentProjectId: project.id
+    };
+    return (<Source
+        id={SOURCE_TODOSOURCE_ID}
+        query={TODOS_FOR_CURRENT_PROJECT_SOURCE_QUERY}
+        typeRelationships={TODOS_FOR_CURRENT_PROJECT_RELATIONSHIPS}
+        parameters={parameters}
+    >
+        {({loading, error, data, updateSourceAfterCreateAction, updateSourceAfterUpdateAction}) => {
+            if (loading) return 'Loading...';
+
+            if (error) return `Error: ${error.graphQLErrors}`;
+
+            const todos = data.sourceData.map(el => ({
+                ...el.instance,
+                isCompleted: el.children[0].instance,
+            }));
+
+            return (
+                <StyleWrapper>
+                    {todos.map(todo => (
+                        <Todo
+                            key={todo.id}
+                            id={todo.id}
+                            name={todo.value}
+                            isCompleted={todo.isCompleted}
+                            onUpdate={updateSourceAfterUpdateAction}
+                        />
+                    ))}
+                </StyleWrapper>)
+        }
+        }
+    </Source>);
+}
+
+
+export default Todos;
+
