3d0292cf605a8feaa4bd9811eaf0b2f78c7aa4ae
Rem Lampa <rem@remlampa.com>
2019-09-13 17:20:23 +0800

    Swap stack to 361

--- src/config/index.js
+++ src/config/index.js
@@ -1,29 +1,29 @@
-// This file was generated automatically by nostack for the app 'app299'.  If you build a new stack from a template, you should replace it.
+// This file was generated automatically by nostack for the app 'app361'.  If you build a new stack from a template, you should replace it.
 // If you modify your current stack to add actions, sources, or types, it would make sense to add constants here.
 
 // platform id
-export const PLATFORM_ID = 'us-west-2_ek58C8aJb';
+export const PLATFORM_ID = 'us-west-2_SXVUZWSfC';
 
 // action ids
   // CREATE_INSTANCE
-export const CREATE_PROJECT_FOR_PROJECT_SOURCE_ACTION_ID='2981b5e5-3f0f-4b92-aa6e-e30d2a1b8e97';
-export const CREATE_TO_DO_FOR_TO_DO_SOURCE_ACTION_ID='709a520b-652f-4490-a457-5bea93c1ba6d';
-export const CREATE_IS_COMPLETED_FOR_TO_DO_SOURCE_ACTION_ID='f65c0c32-ca8e-4852-85ca-62656046f72f';
+export const CREATE_PROJECT_FOR_PROJECT_SOURCE_ACTION_ID='c945d2b5-e3d8-4207-a8bb-f9e0d3a3aac9';
+export const CREATE_TO_DO_FOR_TO_DO_SOURCE_ACTION_ID='77b920ac-198f-4e67-9ba3-cc40c4bb356c';
+export const CREATE_IS_COMPLETED_FOR_TO_DO_SOURCE_ACTION_ID='5f6d0294-144f-45c0-ba2b-ee3c54214fcb';
   // UPDATE_INSTANCE
-export const UPDATE_PROJECT_FOR_PROJECT_SOURCE_ACTION_ID='700a2cf8-505e-4d79-8c82-f912729b2e73';
-export const UPDATE_TO_DO_FOR_TO_DO_SOURCE_ACTION_ID='e3bc26f6-e00e-4677-a2a4-cb93f7f33e4c';
-export const UPDATE_IS_COMPLETED_FOR_TO_DO_SOURCE_ACTION_ID='02dce303-9001-4d4c-babd-1b2ef02c75e1';
+export const UPDATE_PROJECT_FOR_PROJECT_SOURCE_ACTION_ID='d170ccde-3500-49cb-b400-8c4f565ce194';
+export const UPDATE_TO_DO_FOR_TO_DO_SOURCE_ACTION_ID='ef291b40-2b23-40f6-a1be-14194e333e20';
+export const UPDATE_IS_COMPLETED_FOR_TO_DO_SOURCE_ACTION_ID='1f9a8cae-baf0-46e8-bb2a-d0949e11d711';
   // DELETE_INSTANCE
-export const DELETE_PROJECT_FOR_PROJECT_SOURCE_ACTION_ID='c654e083-ccc2-42b8-95b4-20c50ec33fa3';
-export const DELETE_TO_DO_FOR_TO_DO_SOURCE_ACTION_ID='bbb10167-180b-4fc8-a828-1bf323f4061d';
-export const DELETE_IS_COMPLETED_FOR_TO_DO_SOURCE_ACTION_ID='3b2200a6-4709-4659-ba28-2cdc463a7e8c';
+export const DELETE_PROJECT_FOR_PROJECT_SOURCE_ACTION_ID='3ed70ed2-b67d-48c6-abd3-6a33c7e40da4';
+export const DELETE_TO_DO_FOR_TO_DO_SOURCE_ACTION_ID='9912c040-847b-4a65-9cee-d7ace7f04fa7';
+export const DELETE_IS_COMPLETED_FOR_TO_DO_SOURCE_ACTION_ID='3cf42a69-3094-4479-a635-4e057b7963dd';
 
 // source ids
-export const SOURCE_PROJECT_SOURCE_ID='15c7dd3f-c629-4a50-9d21-42bad1faab0a';
-export const SOURCE_TO_DO_SOURCE_ID='1bc5f241-9b05-45f1-a36d-2494ff6b101a';
+export const SOURCE_PROJECT_SOURCE_ID='b9ef6e3e-5b2f-485a-b567-701c8b9f3593';
+export const SOURCE_TO_DO_SOURCE_ID='e8838445-9193-4d33-a649-80aefb9a63a5';
 
 // type ids
-export const TYPE_USER_ID='b39ac6f2-7272-4c01-b0fb-0f4030bf10b7';
-export const TYPE_PROJECT_ID='b10b2caf-a1c7-4bfa-a6db-ad2a3fc13d59';
-export const TYPE_TO_DO_ID='88fc6deb-3e53-4cc9-952d-d009f687275c';
-export const TYPE_IS_COMPLETED_ID='47d44545-8b26-440d-921d-f2021d9616d8';
+export const TYPE_USER_ID='9aade130-c36b-4f4c-9c35-072faf7af4f9';
+export const TYPE_PROJECT_ID='ca7dd808-c637-49dd-8654-af82e9139962';
+export const TYPE_TO_DO_ID='f77b7940-9a24-4df0-a2f1-07f559b8fe24';
+export const TYPE_IS_COMPLETED_ID='ee4e5046-3cb7-4a28-a96b-45c4c3405da4';
