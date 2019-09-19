a6edbe4c68971b91f14ee88a39b7129932d185db
Rem Lampa <rem@remlampa.com>
2019-09-19 19:22:14 +0800

    Updates

---
+++ precache-manifest.188e3541d7ba658811af4f815fc3f4c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486212c34e7dba78caf4d6ec579bae91",
+    "url": "/stackbox-todo/index.html"
+  },
+  {
+    "revision": "e95875bb21f77f6917fe",
+    "url": "/stackbox-todo/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "42c2c864665b72d195a3",
+    "url": "/stackbox-todo/static/js/2.c540fc65.chunk.js"
+  },
+  {
+    "revision": "e95875bb21f77f6917fe",
+    "url": "/stackbox-todo/static/js/main.0ede4538.chunk.js"
+  },
+  {
+    "revision": "fd998375ec4067c6a45b",
+    "url": "/stackbox-todo/static/js/runtime~main.ae73a24c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/stackbox-todo/precache-manifest.309a2aef55607a26821066c1fc85adab.js"
+  "/stackbox-todo/precache-manifest.188e3541d7ba658811af4f815fc3f4c9.js"
 );
 
 self.addEventListener('message', (event) => {
